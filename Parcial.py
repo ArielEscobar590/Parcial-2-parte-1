2d7bbed788ab1b16dc3f63b3089f9313c6e9de0c
Usuario <hugocanizzabala@gmail.com>
2025-09-08 18:46:15 -0600

    ingreso datos

--- Parcial.py
+++ Parcial.py
@@ -1,1 +1,18 @@
-jafjkasjdfkja
+class persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def mostrar(self):
+        print(f"Nombre: {self.nombre}- Edad={self.edad}")
+class candidata(persona):
+    def __init__(self,codigo, nombre, edad,instituto,educativo,municipio):
+        super().__init__(nombre, edad)
+        self.codigo = codigo
+        self.instituto = instituto
+        self.educativo = educativo
+        self.municipio = municipio
+
+    def mostrar(self):
+        print(f"Codigo: {self.codigo}Nombre: {self.nombre}- Edad={self.edad}- Instituto={self.instituto}- Educativo={self.educativo}- Municipio={self.municipio}")
+
