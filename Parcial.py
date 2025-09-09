a03df38e92c2181606e349156f22a5479191cf59
Usuario <hugocanizzabala@gmail.com>
2025-09-08 19:09:20 -0600

    ingreso datos2

--- Parcial.py
+++ Parcial.py
@@ -1,5 +1,6 @@
 
 Dic_candidata={}
+Dic_jurado={}
 class persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
@@ -8,24 +9,27 @@ class persona:
     def mostrar(self):
         print(f"Nombre: {self.nombre}- Edad={self.edad}")
 class candidata(persona):
-    def __init__(self,codigo, nombre, edad,instituto,educativo,municipio):
+    def __init__(self,codigo, nombre, edad,instituto,municipio):
         super().__init__(nombre, edad)
         self.codigo = codigo
         self.instituto = instituto
-        self.educativo = educativo
         self.municipio = municipio
 
     def mostrar(self):
-        print(f"Codigo: {self.codigo}Nombre: {self.nombre}- Edad={self.edad}- Instituto={self.instituto}- Educativo={self.educativo}- Municipio={self.municipio}")
+        print(f"Codigo: {self.codigo}Nombre: {self.nombre}- Edad={self.edad}- Instituto={self.instituto}- Municipio={self.municipio}")
 
 
     def ingrear_candidatas(self):
-        cod=input("Ingrese el codigo de la candidata: ")
+        codigo=input("Ingrese el codigo de la candidata: ")
         nombre=input("Ingrese el nombre: ")
         edad=int(input("Ingrese el edad: "))
         instituto=input("Ingrese el instituto: ")
-        educativo=input("Ingrese el educativo: ")
         municipio=input("Ingrese el municipio: ")
+
+        c=candidata(codigo,nombre,edad,instituto,municipio)
+        c.ingrear_candidatas()
+        Dic_candidata[codigo]=c
+
 
 
 
@@ -38,3 +42,11 @@ class jurador:
     def mostrar(self):
         print(f"Nombre: {self.nombre}- Especialidad={self.especialidad}- Calificar={self.calificar}")
 
+    def ingreso_jurado(self):
+        nombre=input("Ingrese el nombre: ")
+        especialidad=input("Ingrese el especialidad: ")
+        calificar=int(input("Ingrese el calificar: "))
+
+        j=jurador(nombre,especialidad,calificar)
+        j.ingreso_jurado()
+        Dic_jurado[nombre]=j
